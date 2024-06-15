227c9d2eda9aafa259a4b53f7ea978509e8be5cf
jcardus <asklocation.net@gmail.com>
2024-06-15 23:41:34 +0100

    proxy to traccar server

--- build.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/traccar/traccar-web
-cd traccar-web
-npm install
-npm run build
-
--- functions/api/[[api]].js
+++
@@ -1,5 +0,0 @@
-export function onRequest(context) {
-    const url = new URL(context.request.url)
-    url.host = 'traccar-eu.fleetmap.pt'
-    return fetch(new Request(url, context.request))
-}
