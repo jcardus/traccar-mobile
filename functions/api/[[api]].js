642d66da8188834499f327977e29061184a044e2
jcardus <asklocation.net@gmail.com>
2024-06-15 20:27:03 +0100

    proxy to traccar server

--- functions/api/[[api]].js
+++ functions/api/[[api]].js
@@ -1,5 +1,5 @@
 export function onRequest(context) {
     const url = new URL(context.request.url)
-    url.host = 'traccar-eu.fleetmap.io'
+    url.host = 'traccar-eu.fleetmap.pt'
     return fetch(new Request(url, context.request))
 }
