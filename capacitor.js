40719d3a8038af58b882fda5d6a20c1946da31f9
jcardus <asklocation.net@gmail.com>
2024-06-16 03:49:05 +0100

    intercept fetch VITE_TRACCAR_SERVER

--- capacitor.js
+++ capacitor.js
@@ -1,1 +1,9 @@
-alert('hello world')
+// intercept api requests
+const oldFetch = window.fetch;
+window.fetch = async (url, options) => {
+    if (typeof url === 'string' && url.startsWith('/api')) {
+        url = `https://${import.meta.env.VITE_TRACCAR_SERVER}${url}`;
+        return oldFetch(url, options);
+    }
+    return oldFetch(url, options);
+};
