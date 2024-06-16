bfbb24a4d887a84b889da7acc883465a813f6e53
jcardus <asklocation.net@gmail.com>
2024-06-16 04:27:49 +0100

    CapacitorHttp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,11 @@ const config: CapacitorConfig = {
     webDir: 'build',
     ios: {
         contentInset: 'always'
+    },
+    plugins: {
+        CapacitorHttp: {
+            enabled: true
+        }
     }
 };
 
