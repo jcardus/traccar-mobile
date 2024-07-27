159fe62ecc5826afb9f9c6f572e30c6a055ac596
jcardus <asklocation.net@gmail.com>
2024-07-27 05:05:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 rm -rf traccar-web
-git clone --depth 1 https://github.com/traccar/traccar-web
+git clone --depth 1 https://github.com/traccar/traccar-web || true
 cp -v capacitor.config.ts traccar-web
 cp -vr src traccar-web
 {
   echo "import './capacitor.js';"
   cat traccar-web/src/index.jsx
 } > temp && mv temp traccar-web/src/index.jsx
+cd traccar-web || exit
+npm install -D @capacitor/cli @capacitor/core @capacitor/ios typescript
+npm run build
