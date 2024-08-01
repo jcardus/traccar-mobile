1a0efcae4c6231d3d463988c5b44cafdb4a64da8
jcardus <joaquim.cardeira@gmail.com>
2024-08-01 22:47:57 +0100

    generate assets

--- build-ios.sh
+++ build-ios.sh
@@ -1,6 +1,8 @@
 cd traccar-web || exit
 npx cap add ios
+mkdir assets
+cp -v ../assets/* assets
 npx capacitor-assets generate
-mkdir -p ios/App/fastlane
-cp ../fastlane/* ios/App/fastlane
+mkdir ios/App/fastlane ios/App/assets
+cp -v ../fastlane/* ios/App/fastlane
 
